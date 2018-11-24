3762f6ec9f80678fb3598f3fce74c45c5d268458
Noodles <me@xiongty.com>
2018-11-24 16:33:46 +0800

    dispatch 后所有监听的页面都 setData

--- assets/plugins/wx-store/index.js
+++ assets/plugins/wx-store/index.js
@@ -1,25 +1,23 @@
 module.exports = class Store {
-  constructor(option = {}) {
-    this.state = option.state || {}
-    this.actions = option.actions || {}
-  }
-
-  // 修改state的方法
-  setState(data) {
-    this.state = Object.assign({}, this.state, data)
+  constructor({state, actions}) {
+    this.state = state || {}
+    this.actions = actions || {}
+    this.ctxs = []
   }
 
   // 派发action, 统一返回promise action可以直接返回state
-  dispatch(type, ctx, payload) {
-    let update = res => {
+  dispatch(type, payload) {
+    const update = res => {
       if (typeof res !== 'object') return
       this.setState(res)
-      if (!ctx || typeof ctx.setData !== 'function') return
-      ctx.setData(res)
+      this.ctxs.map(ctx => {
+        if (!ctx || typeof ctx.setData !== 'function') return
+        ctx.setData(res)
+      })
     }
 
     if (typeof this.actions[type] !== 'function') return
-    let res = this.actions[type](this, payload)
+    const res = this.actions[type](this, payload)
     if (res.constructor.toString().match(/function\s*([^(]*)/)[1] === 'Promise') return res.then(update)
     else return new Promise(resolve => {
       update(res)
@@ -27,17 +25,20 @@ module.exports = class Store {
     })
   }
 
+  // 修改state的方法
+  setState(data) {
+    this.state = {...this.state, ...data}
+  }
+
   // 根据keys获取state
   getState(keys) {
-    let state = this.state
-    let data = {}
-    keys.forEach(key => data[key] = state[key])
-    return data
+    return keys.reduce((acc, key) => ({...acc, ...{[key]: this.state[key]}}), {})
   }
 
   // 映射state到实例中，可在onload或onshow中调用
   mapState(keys, ctx) {
     if (!ctx || typeof ctx.setData !== 'function') return
     ctx.setData(this.getState(keys))
+    this.ctxs.push(ctx)
   }
 }
